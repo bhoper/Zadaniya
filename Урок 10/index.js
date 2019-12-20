39ca454b85eaed9289bab142cd91c9bd096a68d4
bhoper <55310665+bhoper@users.noreply.github.com>
2019-12-20 19:35:43 +0300

    Add files via upload

---
+++ Урок 10/index.js
@@ -0,0 +1,29 @@
+//function greeteng(name="gg") {
+//	console.log(`hello ${name}`);
+//}
+
+//greeteng("oleg");
+//greeteng();
+//function checkage (age,text)
+const checkage=function(age,text)
+ {
+	if (age<18){
+		return text;
+	} else{
+		return=true;
+	}
+	
+}
+function authorithation(text=`access denied`function ){
+
+const age=+prompt(`Enter your age`);
+if function(age,text)
+	console.log(text);
+}else
+{
+	
+}
+
+
+
+
---
+++ Урок 12/index.js
@@ -0,0 +1,84 @@
+// 	console.log('ok');
+// 	const testFunc= function (name'world')
+// 	{
+// console.log('Hello-World'+name);
+// 	}
+	//setTimeout(
+		/*function()	
+		{
+			alert('Ok');
+		}
+		,2000
+	)*/
+	/*(
+		function(str='ok'){
+			console.log(str);
+		}
+	)('huhbu')*/
+	/*const arrow = (str = 'ok') =>{		
+		console.log(str);
+	}
+	arrow('arrow fun');*/
+	// const gggObj1 = new Object();
+   //  const gggObj2 ={};
+	
+	/*const user = {
+		name: 'Leha',
+		age: 110,
+	};
+	user.salary=1000;
+	const prop1='prip1';
+	user.prop1=prop1;
+	user['key with space']=true;
+	const user2 = user;
+	const user3 = Object.assign({}, user);
+	user.salary=2000
+	
+	console.log(user, user2,user3);
+	console.log ("salary" in user2)
+    delete user.age;
+    console.log(user);
+    for (key in user2){
+    	console.log(key, user2[key])
+    }*/
+    /*const user={
+    	name:'Leva',
+    	age: '-100',
+    	hi:function(){
+    		alert('My nane is ' + user.name);
+    	}
+    };
+    user.hi();*/
+    // user.hi()
+/*const hi= function(){
+	alert('hello ' + this.name);
+}
+const Alice = {
+	name:'Alice',
+}
+const Diman = {
+	name:'Diman',
+}
+Alice.hi = hi;
+Diman.hi = hi;
+Magamed.hi = hi;
+Alice.hi();
+Diman.hi();*/
+/*const Magamed = {
+	name:'Magamed',
+	hi: function (){
+		const arrow = () => {
+			alert('My name is ' + this.name);
+		}
+		arrow();
+	}
+}
+Magamed.hi();*/
+const Calculator={
+let PowerCalc.AddMedhod("+"(a,b)=>a+b);
+let PowerCalc.AddMedhod("-"(a,b)=>a-b);
+let PowerCalc.AddMedhod("*"(a,b)=>a*b);
+let PowerCalc.AddMedhod("/"(a,b)=>a/b);
+let result = PowerCalc.Calculator;
+alert (result);
+}
